e8fa3cdb039d07415bb1a8cce22dc9dfe580d720
Naz <naseemhammoud@gmail.com>
2017-02-24 11:27:35 -0800

    Added base files

---
+++ Clocker/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Clocker.Startup))]
+namespace Clocker
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
